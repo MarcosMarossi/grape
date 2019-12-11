b7151258f173961ea4b7d98957640667bc572d96
Marcos Marossi <marcos.marossi@fatec.sp.gov.br>
2019-12-11 11:52:15 -0300

    Add main activity with buttons of register and login.

--- app/src/main/java/com/example/grapeproject/MainActivity.java
+++ app/src/main/java/com/example/grapeproject/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.grapeproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        getWindow().setStatusBarColor( Color.parseColor( "#673AB7" ));
+    }
+
+    public void makeRegister(View view){
+        startActivity( new Intent(getApplicationContext(), RegisterActivity.class));
+    }
+
+    public void openLogin(View view){
+        startActivity( new Intent(getApplicationContext(), LoginActivity.class));
     }
 }
